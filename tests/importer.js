2279ae91697df598abc6e3a0265379d7f7f4dddd
Carlo Panzi <carlo.panzi@gmail.com>
2018-04-30 16:59:00 +0200

    Importer can auto-select the xml format

--- tests/importer.js
+++ tests/importer.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import { importer, normalizeDive } from '../lib/importer/divelog';
+import { importer } from '../lib/importer';
 
 test('Simple dive', async t => {
     const expected = {
