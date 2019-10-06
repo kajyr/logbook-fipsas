5b8d8c2e188a5e81fea1d45da84d9fd74ceb1bfa
Carlo <carlo.panzi@gmail.com>
2019-10-06 21:17:03 +0200

    chore: Old mustache templates cleanup

--- templates/fipsas-didattica/build-css.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-../../node_modules/node-sass/bin/node-sass --include-path src src/style.scss ./style.css
-../../node_modules/node-sass/bin/node-sass --include-path src src/print.scss ./print.css
